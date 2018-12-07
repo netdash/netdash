ca5d772ec8d7f5592661cf7373243d1e5a0f2288
Kris Steinhoff <steinhof@umich.edu>
2018-12-07 16:28:41 -0500

    Add config checking and errors

--- netdash/netdash_api/urls.py
+++ netdash/netdash_api/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 from importlib import import_module
 from rest_framework import routers
@@ -15,6 +16,11 @@ try:
     except NameError:
         pass
 except AttributeError:
-    pass
+    raise ImproperlyConfigured('NETDASH_API_DEVICE_PROVIDER must be set')
+except ModuleNotFoundError:
+    raise ImproperlyConfigured(
+        'Could not import views from %s (Is that package installed?) '
+        '(Specificed in NETDASH_API_DEVICE_PROVIDER.)' %
+        settings.NETDASH_API_DEVICE_PROVIDER)
 
 urlpatterns = router.urls
