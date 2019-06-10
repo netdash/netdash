4a4924a7f625e7d04a5c19fde5f29f8a2dc860d0
James Ostrander <jlost@umich.edu>
2019-06-10 14:42:58 -0400

    Accomodate ND modules without SETTINGS_FROM_ENV

--- src/netdash/netdash/utils.py
+++ src/netdash/netdash/utils.py
@@ -3,14 +3,11 @@ import importlib
 
 def get_module_slug(module_name):
     module = importlib.import_module(module_name)
-    module_slug = getattr(module, 'SLUG', None)
-    if module_slug:
-        return module_slug
-    module_slug = module_name.replace('netdash_', '')
-    module_slug = module_slug.split('_')[0]
-    return module_slug
+    if hasattr(module, 'SLUG'):
+        return module.SLUG
+    return module_name.replace('netdash_', '').split('_')[0]
 
 
 def get_module_settings(module_name):
     module = importlib.import_module(module_name)
-    return module.SETTINGS_FROM_ENV
+    return getattr(module, 'SETTINGS_FROM_ENV', [])
