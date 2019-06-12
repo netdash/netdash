b9258e79f9091e6b02e5603658766e4385120667
James Ostrander <jlost@umich.edu>
2019-06-12 08:52:53 -0400

    Add schema_view

--- src/netdash/netdash_api/urls.py
+++ src/netdash/netdash_api/urls.py
@@ -4,18 +4,22 @@ from importlib import import_module
 from django.urls import path
 from django.conf import settings
 from django.conf.urls import include, url
+
+from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.views import get_swagger_view
 
 # app_name = 'netdash_api'
-schema_view = get_swagger_view(title='NetDash API')
+swagger_view = get_swagger_view(title='NetDash API')
+schema_view = get_schema_view(title='NetDash API')
 
 
 def get_url(module_name):
     slug = settings.NETDASH_MODULE_SLUGS[module_name]
     return url(r'^' + slug + '/', include(f'{module_name}.api.urls'))
 
-module_urlpatterns = [ get_url(module_name) for module_name in settings.NETDASH_MODULE_SLUGS]
+module_urlpatterns = [ get_url(module_name) for module_name in settings.NETDASH_MODULE_SLUGS ]
 
 urlpatterns = module_urlpatterns + [
-    path('schema/', schema_view),
+    path('swagger/', swagger_view),
+    path('', schema_view),
 ]
