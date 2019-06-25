592747d3323444391598c5a28e4c7dd3df424a82
James Ostrander <jlost@umich.edu>
2019-06-25 10:19:17 -0400

    Accurate warnings if urls.py | api/urls.py missing

--- src/netdash/netdash/utils.py
+++ src/netdash/netdash/utils.py
@@ -1,6 +1,9 @@
 import importlib
+import logging
 
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 def flatten(l): return [item for sublist in l for item in sublist]
@@ -10,10 +13,12 @@ def get_module_slug(module_name):
     try:
         module = importlib.import_module(f'{module_name}.urls')
     except ModuleNotFoundError as e1:
+        logger.warning(f'Failed to import urls.py for module: {module_name}')
+        logger.info(e1, exc_info=True)
         try:
             module = importlib.import_module(f'{module_name}.api.urls')
         except ModuleNotFoundError as e2:
-            raise Exception(f'Module has neither urls.py nor api/urls.py: {module_name}') from e2
+            raise Exception(f'Failed to import both urls.py and api/urls.py for module: {module_name}') from e2
     if hasattr(module, 'app_name'):
         if module.app_name.endswith('-api'):
             return module.app_name.replace('-api', '')
