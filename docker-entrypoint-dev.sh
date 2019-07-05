21545cd7eeb63a9946284f1bf477f87c1cb8ecf9
Rajiv Bharadwaj <rajivbh@umich.edu>
2019-07-05 12:09:53 -0400

    NETDASH_MODULES in default if not in user-settings

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -6,8 +6,9 @@ cd ../..
 
 for app in $(ls -d apps_dev/*/); do
     app_name=$(grep "name=" ${app}/setup.py | sed -E -e "s/name=|[\'\,]//g" -e "s/[\-]/\_/g")
+    modules=$(grep "NETDASH_MODULES=" .user-settings.env || grep "NETDASH_MODULES=" .defaults.env)
 
-    if `grep "NETDASH_MODULES" .user-settings.env | grep -q  ${app_name}`
+    if [[ $(echo "${modules}" | grep ${app_name}) ]]
     then
         pip3 install -e ${app} || echo "$(basename ${0}): couldn't install ${app}"
     fi
