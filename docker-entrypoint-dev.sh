e114940d92c9a1c9fe4cd8c6d0b027b670c3907c
Rajiv Bharadwaj <rajivbh@umich.edu>
2019-07-05 11:00:41 -0400

    Install modules that are in NETDASH_MODULES The only problem to check for is when there's no modules set in user settings. In this case, the program must fall back to defaults.env

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -5,7 +5,13 @@ set -e
 cd ../..
 
 for app in $(ls -d apps_dev/*/); do
-    pip3 install -e ${app} || echo "$(basename ${0}): couldn't install ${app}"
+    app_name=$(grep "name=" ${app}/setup.py | sed -E -e "s/name=|[\'\,]//g" -e "s/[\-]/\_/g")
+
+    if `grep "NETDASH_MODULES" .user-settings.env | grep -q  ${app_name}`
+    then
+        pip3 install -e ${app} || echo "$(basename ${0}): couldn't install ${app}"
+    fi
+
 done
 
 cd src/netdash
