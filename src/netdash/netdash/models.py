3ff66730e36aaf32b2f0096ce110d3879469141b
James Ostrander <jlost@umich.edu>
2019-07-22 10:46:59 -0400

    Resolve tuples in groups

--- src/netdash/netdash/models.py
+++ src/netdash/netdash/models.py
@@ -7,8 +7,8 @@ class User(AbstractUser):
         self._remove_revoked_group_memberships(group_names)
 
     def _ensure_groups_created_and_added(self, group_names):
-        groups = [Group.objects.get_or_create(name=g) for g in group_names]
-        self.groups.add(groups)
+        groups = [Group.objects.get_or_create(name=g)[0] for g in group_names]
+        self.groups.add(*groups)
 
     def _remove_revoked_group_memberships(self, group_names):
         revoked_groups = self.groups.exclude(name__in=group_names)
