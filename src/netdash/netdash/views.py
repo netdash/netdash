4adf64dbd55a809d5c039a366949130fc7b3f629
James Ostrander <jlost@umich.edu>
2019-07-23 08:44:47 -0400

    Add UI elements to log in/out

---
+++ src/netdash/netdash/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect(reverse('index'))
