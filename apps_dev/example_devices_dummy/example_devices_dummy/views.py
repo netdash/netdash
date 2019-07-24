4e75bbab45eb716bd194d4f077df24e1b280ab70
James Ostrander <jlost@umich.edu>
2019-07-24 15:24:42 -0400

    Add example of can_view_module in view

--- apps_dev/example_devices_dummy/example_devices_dummy/views.py
+++ apps_dev/example_devices_dummy/example_devices_dummy/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 
+@permission_required("example_devices_dummy.can_view_module", raise_exception=True)
 def index(request):
     return render(request, "index.html")
