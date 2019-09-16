3425b307a01e41bc5f18dd486d909a4d79486e42
James Ostrander <jlost@umich.edu>
2019-09-16 17:55:00 +0000

    Revert NetdashConfig ready

--- src/netdash/netdash/apps.py
+++ src/netdash/netdash/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
 from django.conf import settings
 
-import .utils
-
 
 class NetdashConfig(AppConfig):
     name = 'netdash'
-
-    def ready(self):
-        setattr(settings, 'NETDASH_MODULE_OBJECTS', utils.create_netdash_modules()
