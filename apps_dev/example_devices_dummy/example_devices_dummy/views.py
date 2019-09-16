cbe8e3de8afbb263faea3fa405ce0ebc3151ef0d
James Ostrander <jlost@umich.edu>
2019-09-16 17:54:46 +0000

    Fix templates & route namespacing in examples

--- apps_dev/example_devices_dummy/example_devices_dummy/views.py
+++ apps_dev/example_devices_dummy/example_devices_dummy/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import permission_required
 
 @permission_required("example_devices_dummy.can_view_module", raise_exception=True)
 def index(request):
-    return render(request, "index.html")
+    return render(request, "example_devices_dummy/index.html")
