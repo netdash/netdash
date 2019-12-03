3ddeef20d91f1ee77680798f064a4ef69aa114cb
James Ostrander <jlost@umich.edu>
2019-12-03 19:49:05 +0000

    Add combined hostlookup tests

--- netdash/hostlookup_combined/tests.py
+++ netdash/hostlookup_combined/tests.py
@@ -3,6 +3,196 @@ from django.test import TestCase
 from .merge import MergedTable, MergedRow, MergedCell, SourceValue
 from hostlookup_combined.templatetags.merge_tags import merged_table, merged_cell
 
+from typing import Union, List, Dict
+from ipaddress import IPv4Address, IPv6Address, ip_network, ip_address
+from unittest.mock import patch
+import urllib
+import datetime
+
+from django.urls import reverse
+from django.contrib.auth.models import Permission
+from django.test.client import RequestFactory
+
+from netdash.models import User
+from bluecat_bam import BAM
+from hostlookup_bluecat.bluecat import BlueCatNetwork, BlueCatAddress, BlueCatConfiguration
+from hostlookup_combined.views import HostLookupView
+
+from netaddr import EUI
+
+
+def mock_lookup_cidr(conn: BAM, ip: Union[IPv4Address, IPv6Address], container_bcid: int) -> BlueCatNetwork:
+    nw = ip_network('141.100.0.0/30')
+    return BlueCatNetwork(
+        777,
+        'my_network',
+        nw,
+        ip_address('10.10.10.10'),
+        [
+            BlueCatAddress(
+                0,
+                'first',
+                nw[0],
+                'DHCP_RESERVED',
+                EUI('96:32:80:e1:3b:cf'),
+                ['example.com', 'second.com'],
+            ),
+            BlueCatAddress(
+                1,
+                'second',
+                nw[1],
+                'DHCP_RESERVED',
+                EUI('ee:03:a8:69:97:00'),
+                [],
+            ),
+            BlueCatAddress(
+                2,
+                'third',
+                nw[2],
+                'DHCP_RESERVED',
+                EUI('9e:2c:f0:5e:c3:3f'),
+                [],
+            ),
+            BlueCatAddress(
+                3,
+                'fourth',
+                nw[3],
+                'DHCP_RESERVED',
+                EUI('22:07:bb:ac:50:df'),
+                [],
+            ),
+        ]
+    )
+
+
+def mock_lookup_cidr_empty(conn: BAM, ip: Union[IPv4Address, IPv6Address], container_bcid: int) -> BlueCatNetwork:
+    nw = ip_network('141.100.0.0/30')
+    return BlueCatNetwork(
+        777,
+        'my_network',
+        nw,
+        ip_address('10.10.10.10'),
+        [],
+    )
+
+
+def mock_lookup_configurations(conn: BAM) -> List[BlueCatConfiguration]:
+    return [
+        BlueCatConfiguration(
+            1,
+            'default_config',
+            'The default configuration.',
+            None,
+        ),
+        BlueCatConfiguration(
+            2,
+            'other_config',
+            'Another configuration.',
+            None,
+        ),
+    ]
+
+
+def mock_fetch_as_dicts(cursor) -> List[Dict]:
+    return [
+        {
+            'mac': '96:32:80:e1:3b:cf',
+            'ip': '141.100.0.0',
+            'name': 's-BLDG-1',
+            'switch': '10.10.0.0',
+            'time_last': datetime.datetime(2019, 12, 1),
+            'location': 'Big Building F1',
+        },
+        {
+            'mac': 'ee:03:a8:69:97:00',
+            'ip': '141.100.0.1',
+            'name': 's-BLDG-1',
+            'switch': '10.10.0.0',
+            'time_last': datetime.datetime(2019, 12, 1),
+            'location': 'Big Building F1',
+        },
+    ]
+
+
+@patch('hostlookup_bluecat.views.get_connection')
+@patch('hostlookup_combined.utils.get_connection')
+@patch('hostlookup_netdisco.utils.connections')
+@patch('hostlookup_netdisco.utils.fetch_as_dicts', mock_fetch_as_dicts)
+@patch('hostlookup_bluecat.views.lookup_configurations', mock_lookup_configurations)
+@patch('hostlookup_combined.utils.lookup_cidr', mock_lookup_cidr)
+class HostlookupCombinedTests(TestCase):
+
+    def setUp(self):
+        u = User.objects.create_user('networker', password='qwerty')
+        p = Permission.objects.get_by_natural_key('can_view_module', 'hostlookup_combined', 'modulepermissions')
+        # print('permission:', p)
+        u.user_permissions.add(
+            p
+        )
+        # print('user permissions:', u.user_permissions.all())
+
+    def _login(self):
+        self.client.login(username='networker', password='qwerty')
+
+    def _get_index(self):
+        return self.client.get(reverse('hostlookup_combined:index'))
+
+    def _get_index_view(self, **kwargs):
+        f = RequestFactory()
+        params = urllib.parse.urlencode(kwargs)
+        r = f.get(f'{reverse("hostlookup_combined:index")}?{str(params)}')
+        r.user = User.objects.get_by_natural_key('networker')
+        return HostLookupView.as_view()(r)
+
+    @patch('hostlookup_combined.utils.lookup_cidr', mock_lookup_cidr_empty)
+    def test_renders_form(self, views_get_connection, utils_get_connection, connections):
+        response = self._get_index_view()
+        response.render()
+        self.assertContains(response, '<form')
+        self.assertContains(response, '<button type="submit"')
+        self.assertContains(
+            response,
+            '<select name="bluecat_config" class="col-lg-3 custom-select" value="">'
+        )
+        self.assertContains(
+            response,
+            '<input type="text"'
+        )
+        self.assertContains(response, '<option value="1"')
+        self.assertContains(response, '<option value="2"')
+
+    def test_renders_form_selections(self, views_get_connection, utils_get_connection, connections):
+        response = self._get_index_view(q='10.10.10.10', bluecat_config='1')
+        response.render()
+        self.assertContains(
+            response,
+            '<select name="bluecat_config" class="col-lg-3 custom-select" value="1">'
+        )
+        self.assertContains(
+            response,
+            '<input type="text" class="form-control" name="q" placeholder="Search..." value="10.10.10.10" />'
+        )
+
+    def test_renders_results(self, views_get_connection, utils_get_connection, connections):
+        response = self._get_index_view(q='10.10.10.10', bluecat_config='1')
+        response.render()
+        self.assertContains(response, '<table')
+        self.assertContains(response, '<th>IPv4</th>')
+        self.assertContains(response, '141.100.0.0')
+        # Renders order for IPv4 141.100.0.1
+        self.assertContains(response, '<td class="valid" data-order="2372141057">')
+        self.assertContains(response, 'example.com')
+        self.assertContains(response, 'second.com')
+
+    def test_renders_ip_error(self, views_get_connection, utils_get_connection, connections):
+        response = self._get_index_view(q='foobar', bluecat_config='1')
+        response.render()
+        self.assertContains(response, 'does not appear to be an IPv4 or IPv6 address')
+
+    def test_view_permission_required(self, views_get_connection, utils_get_connection, connections):
+        response = self._get_index()
+        self.assertEqual(response.status_code, 403)
+
 
 class MergedCellTestCase(TestCase):
     def setUp(self):
