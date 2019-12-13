67ddd294229180fa64456e39a8b3647ac2a4f44d
James Ostrander <jlost@umich.edu>
2019-12-13 18:08:15 +0000

    Make IPs optional in HostLookupResponse

--- netdash/hostlookup_abstract/api/serializers.py
+++ netdash/hostlookup_abstract/api/serializers.py
@@ -3,10 +3,10 @@ from rest_framework import serializers
 
 class HostLookupResponseSerializer(serializers.Serializer):
     mac = serializers.CharField()
-    ipv4 = serializers.IPAddressField()
+    ipv4 = serializers.IPAddressField(required=False)
     ipv6 = serializers.IPAddressField(required=False)
     last_seen = serializers.DateTimeField()
     host_port_name = serializers.CharField()
-    switch_ipv4 = serializers.IPAddressField()
+    switch_ipv4 = serializers.IPAddressField(required=False)
     switch_ipv6 = serializers.IPAddressField(required=False)
     switch_location = serializers.CharField()
