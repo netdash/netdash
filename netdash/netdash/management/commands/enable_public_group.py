216c25b7220365bcf1dc1369197d9af7c4ed45a3
Rajiv Bharadwaj <rajivbh@umich.edu>
2020-01-10 18:59:20 -0500

    Added optional parameter to prevent all users from getting added

--- netdash/netdash/management/commands/enable_public_group.py
+++ netdash/netdash/management/commands/enable_public_group.py
@@ -4,21 +4,23 @@ Create a Public Group where each user gets viewing access to all modules
 import logging
 
 from django.conf import settings
-from django.contrib.auth.models import Group, Permission
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     help = 'Creates a Public group and gives viewing permissions to all users'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--nousers', action='store_true',
+                            help='Don\'t add all users by default to the group')
+
     def handle(self, *args, **options):
         GROUPNAME = "Public"
         PERMISSION_CODENAME = "can_view_module"
 
         new_group, created = Group.objects.get_or_create(name=GROUPNAME)
-        from django.apps import apps
-        print(apps.get_app_configs())
         for module in settings.NETDASH_MODULES:
             try:
                 viewing_permission = Permission.objects.get(content_type__app_label=module,
@@ -30,9 +32,9 @@ class Command(BaseCommand):
 
         logging.info("Created Public group with viewing permissions")
 
-        User = get_user_model()
+        if not options['nousers']:
+            User = get_user_model()
+            users = User.objects.all()
 
-        users = User.objects.all()
-
-        for user in users:
-            new_group.user_set.add(user)
+            for user in users:
+                new_group.user_set.add(user)
