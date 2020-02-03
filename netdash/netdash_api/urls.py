aa84ebe7c65286ae23f1f88e89b4126041793472
James Ostrander <jlost@umich.edu>
2020-02-03 18:32:59 +0000

    Remove is_staff req from yasg views

--- netdash/netdash_api/urls.py
+++ netdash/netdash_api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, re_path
 from django.conf import settings
-from django.contrib.admin.views.decorators import staff_member_required
 
 from rest_framework.schemas import get_schema_view
 from drf_yasg.views import get_schema_view as get_yasg_view
@@ -22,15 +21,10 @@ schema_view = get_schema_view(title='NetDash API')
 module_urlpatterns = [module.api_url for module in NETDASH_MODULES if module.api_url]
 
 urlpatterns = module_urlpatterns + [
-    re_path(
-        r'^schema(?P<format>\.json|\.yaml)$',
-        staff_member_required(yasg_view.without_ui(cache_timeout=0)),
-        name='schema-json'
-    ),
-    path('swagger', staff_member_required(yasg_view.with_ui('swagger', cache_timeout=0)), name='swagger'),
-    path('redoc', staff_member_required(yasg_view.with_ui('redoc', cache_timeout=0)), name='redoc'),
-    path('', staff_member_required(schema_view), name='schema'),
-    # TODO: Wrap API views with drf-yasg for metadata?
+    re_path(r'^schema(?P<format>\.json|\.yaml)$', yasg_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger', yasg_view.with_ui('swagger', cache_timeout=0), name='swagger'),
+    path('redoc', yasg_view.with_ui('redoc', cache_timeout=0), name='redoc'),
+    path('', schema_view, name='schema'),
     re_path(r'^o/token/$', TokenView.as_view(), name="token"),
     re_path(r'^o/revoke_token/$', RevokeTokenView.as_view(), name="revoke-token"),
 ]
